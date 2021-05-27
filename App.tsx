6eb5060f9c0534cc91b2ee2f2ae594cec514ad05
Sham <hisham@alphapico.com>
2021-05-27 20:53:46 +0800

    implement api and user model

--- App.tsx
+++ App.tsx
@@ -1,18 +1,81 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
+import { Auth, API, graphqlOperation } from "aws-amplify";
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
+
 import Amplify from "aws-amplify";
 import config from "./src/aws-exports";
+import { withAuthenticator } from "aws-amplify-react-native";
 Amplify.configure(config);
 
-export default function App() {
+const randomImages = [
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg",
+];
+
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return randomImages[Math.floor(Math.random() * randomImages.length)];
+  };
+
+  // run this snippet only when App is first mounted
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      if (userInfo) {
+        const userData = await API.graphql(
+          graphqlOperation(getUser, { id: userInfo.attributes.sub })
+        );
+
+        // console.log(userData);
+        //  {
+        //   "data": Object {
+        //     "getUser": Object {
+        //       "createdAt": "2021-05-27T10:03:52.076Z",
+        //       "id": "e0a59b62-75db-4201-a520-b1b2f5fd6828",
+        //       "imageUri": "https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg",
+        //       "name": "sham",
+        //       "status": "Hey, I am using WhatsApp",
+        //       "updatedAt": "2021-05-27T10:03:52.076Z",
+        //     },
+        //   },
+        // }
+
+        if (userData.data.getUser) {
+          console.log("User is already registered in database");
+          return;
+        }
+
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: getRandomImage(),
+          status: "Hey, I am using WhatsApp",
+        };
+
+        console.log(newUser);
+
+        await API.graphql(graphqlOperation(createUser, { input: newUser }));
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
@@ -25,3 +88,5 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App);
